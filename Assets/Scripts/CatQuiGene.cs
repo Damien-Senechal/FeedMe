00ced0d73646f28196194fd87b0d0d2d2a67e40b
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2022-11-07 09:51:43 +0100

    add chatquigene

--- Assets/Scripts/CatQuiGene.cs
+++ Assets/Scripts/CatQuiGene.cs
@@ -5,33 +5,51 @@ using UnityEngine;
 public class CatQuiGene : MonoBehaviour
 {
     float timer = 0;
-    bool goUp = false;
+    bool goUp = true;
     private RectTransform rect;
-    public int speed = 1000;
+    public int speed;
 
     // Start is called before the first frame update
     void Start()
     {
         rect = GetComponent<RectTransform>();
-        timer = Random.Range(1, 5);
+        timer = Random.Range(5, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(timer);
+        //Debug.Log(timer);
         timer -= Time.deltaTime;
         if(timer <= 0)
         {
-            goUp = true;
-        }
-
-        if (goUp)
-        {
-            rect.localPosition += Vector3.up * speed * Time.deltaTime;
-            //goUp = false;
-            //timer = Random.Range(1, 5);
+            StartCoroutine(Movement());
         }
         
+    }
+
+    IEnumerator Movement()
+    {
+        if(rect.localPosition.y <= -2385 && !goUp)
+        {
+            timer = Random.Range(5, 10);
+            goUp = true;
+            yield return null;
+        }
+        else if (!goUp)
+        {
+            rect.localPosition += Vector3.down * speed * Time.deltaTime;
+            yield return null;
+        }
+        else if (rect.localPosition.y >= -147 && goUp)
+        {
+            yield return new WaitForSeconds(5f);
+            goUp = false;
+        }
+        else if (goUp)
+        {
+            rect.localPosition += Vector3.up * speed * Time.deltaTime;
+            yield return null;
+        }
     }
 }
